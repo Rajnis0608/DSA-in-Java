72d9d4741cc33bd54421d4f3ff7c57d2590a9271
rajnis0608 <rajnis0608@gmail.com>
2022-02-04 23:37:39 +0530

    Basic recursion

--- Recursion/Basics/src/com/javalearning/PrintNumbers.java
+++ Recursion/Basics/src/com/javalearning/PrintNumbers.java
@@ -10,10 +10,33 @@ public class PrintNumbers {
         print(n-1);
         System.out.print(n + " ");
     }
+
+    public static void print2Mul(int n){
+        if(n == 1) {
+            return;
+        }
+        print2Mul(n-1);
+        if(n%2 == 0){
+            System.out.print(n + " ");
+        }
+    }
+
+    public static void printNto1(int n){
+        if(n == 0 ){
+            return;
+        }
+        System.out.print(n + " ");
+        printNto1(n-1);
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter number : ");
         int x = scanner.nextInt();
         print(x);
+        System.out.println();
+        print2Mul(x);
+        System.out.println();
+        printNto1(x);
     }
 }
